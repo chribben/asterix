dd616143d0a1deb5d5735d9a7a673aca5c282fab
Oskar Wickström <oskar.wickstrom@live.com>
2014-11-10 18:10:49 +0100

    Use lowercase names for types

--- lib/asterix.ex
+++ lib/asterix.ex
@@ -1,25 +1,5 @@
 defmodule Asterix do
 
-  # RequestMessage => ApiKey ApiVersion CorrelationId ClientId RequestMessage
-  #   ApiKey => int16
-  #   ApiVersion => int16
-  #   CorrelationId => int32
-  #   ClientId => string
-  #   RequestMessage => MetadataRequest | ProduceRequest | FetchRequest
-  #                   | OffsetRequest | OffsetCommitRequest | OffsetFetchRequest
-  @type Request :: {ApiKey, ApiVersion, CorrelationId, ClientId, RequestMessage}
-  @type ApiKey :: Integer
-  @type ApiVersion :: Integer
-  @type CorrelationId :: Integer
-  @type ClientId :: char_list
-  @type RequestMessage :: MetadataRequest | ProduceRequest |
-  FetchRequest | OffsetRequest | OffsetCommitRequest | OffsetFetchRequest
-
-  # MetadataRequest => [TopicName]
-  #   TopicName => string
-  @type MetadataRequest :: list(TopicName)
-  @type TopicName :: char_list
-
   # MetadataResponse => [Broker][TopicMetadata]
   #   Broker => NodeId Host Port
   #   NodeId => int32
@@ -33,24 +13,21 @@ defmodule Asterix do
   #   Leader => int32
   #   Replicas => [int32]
   #   Isr => [int32]
-  @type MetadataResponse :: {list(Broker), list(TopicMetadata)}
-  @type Broker :: {NodeId, Host, Port}
-  @type NodeId :: Integer
-  @type Host :: char_list
-  @type Port :: Integer
-  @type TopicMetadata :: {TopicErrorCode, TopicName, list(PartitionMetadata)}
-  @type PartitionErrorCode :: Integer
-  @type PartitionId :: Integer
-  @type Leader :: Integer
-  @type Replicas :: list(Integer)
-  @type Isr :: list(Integer)
-
-  # MessageSet => [Offset MessageSize Message]
-  #   Offset => int64
-  #   MessageSize => int32
-  @type MessageSet :: list({Offset, MessageSize, Message})
-  @type Offset :: Integer
-  @type MessageSize :: Integer
+  @type metadata_response :: {list(broker), list(topic_metadata)}
+  @type broker :: {node_id, broker_host, broker_port}
+  @type node_id :: integer
+  @type broker_host :: char_list
+  @type broker_port :: integer
+  @type topic_metadata :: {topic_error_code, topic_name,
+                           list(partition_metadata)}
+  @type topic_error_code :: integer
+  @type partition_metadata :: {partition_error_code, partition_id, leader,
+                              replicas, isr}
+  @type partition_error_code :: integer
+  @type partition_id :: integer
+  @type leader :: integer
+  @type replicas :: list(integer)
+  @type isr :: list(integer)
 
   # Message => Crc MagicByte Attributes Key Value
   #   Crc => int32
@@ -58,28 +35,58 @@ defmodule Asterix do
   #   Attributes => int8
   #   Key => bytes
   #   Value => bytes
-  @type Message :: {Crc, MagicByte, Attributes, Key, Value}
-  @type Crc :: Integer
-  @type MagicByte :: Integer
-  @type Attributes :: Integer
-  @type Key :: binary
-  @type Value :: binary
+  @type message :: {crc, magic_byte, attributes, key, value}
+  @type crc :: integer
+  @type magic_byte :: integer
+  @type attributes :: integer
+  @type key :: binary
+  @type value :: binary
+
+  # MessageSet => [Offset MessageSize Message]
+  #   Offset => int64
+  #   MessageSize => int32
+  @type message_set :: list({offset, message_size, message})
+  @type offset :: integer
+  @type message_size :: integer
 
   # ProduceRequest => RequiredAcks Timeout [TopicName [Partition MessageSetSize MessageSet]]
   #   RequiredAcks => int16
   #   Timeout => int32
   #   Partition => int32
   #   MessageSetSize => int32
-  @type ProduceRequest :: {RequiredAcks, Timeout,
-                           list({TopicName, list({Partition,
-                                                  MessageSetSize,
-                                                  MessageSet})})}
-  @type RequiredAcks :: Integer
-  @type Timeout :: Integer
-  @type Partition :: Integer
-  @type MessageSetSize :: Integer
+  @type produce_request :: {required_acks, produce_timeout,
+                            list({topic_name, list({partition,
+                                                    message_set_size,
+                                                    message_set})})}
+  @type required_acks :: integer
+  @type produce_timeout :: integer
+  @type partition :: integer
+  @type message_set_size :: integer
 
+  # MetadataRequest => [TopicName]
+  #   TopicName => string
+  @type metadata_request :: list(topic_name)
+  @type topic_name :: char_list
 
-  # TODO: MOOOAAAR TYPEZZZZZZ
+  # RequestMessage => ApiKey ApiVersion CorrelationId ClientId RequestMessage
+  #   ApiKey => int16
+  #   ApiVersion => int16
+  #   CorrelationId => int32
+  #   ClientId => string
+  #   RequestMessage => MetadataRequest | ProduceRequest | FetchRequest
+  #                   | OffsetRequest | OffsetCommitRequest | OffsetFetchRequest
+  @type request :: {api_key, api_version, correlation_id, client_id, request_message}
+  @type api_key :: integer
+  @type api_version :: integer
+  @type correlation_id :: integer
+  @type client_id :: char_list
+  @type request_message :: metadata_request # | ...
+
+  # TODO: moar typez
+
+  @spec get_metadata() :: metadata_response
+  def get_metadata() do
+    nil
+  end
 
 end
